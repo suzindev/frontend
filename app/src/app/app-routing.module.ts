28c80cd67b85b135e622acf87bd0a2f561a1dfe6
Fernando Suzin <suzin.dev@gmail.com>
2022-10-08 19:50:24 -0300

    Estilizando layout.

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
+import { NewTeamComponent } from './components/pages/new-team/new-team.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent
-  }
+  { path: '', component: HomeComponent },
+  { path: 'team/new', component: NewTeamComponent }
 ];
 
 @NgModule({
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { NewTeamComponent } from './components/pages/new-team/new-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NewTeamComponent
   ],
   imports: [
     BrowserModule,
